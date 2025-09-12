0548e578b9240ac3e34d81a0e4ba5f08fcc1193e
Neeraali-digital <neeraalidigitalweb@gmail.com>
2025-09-12 17:26:00 +0530

    .

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import 'zone.js';
 
 bootstrapApplication(AppComponent)
   .catch(err => console.error(err));
